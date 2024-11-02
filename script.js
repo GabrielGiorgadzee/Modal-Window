fd6b9a681b8a8d1fa7009a3793ab4b19719112d7
GabroSH <gabroaz8@gmail.com>
2024-11-02 21:02:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button Clicked');
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
